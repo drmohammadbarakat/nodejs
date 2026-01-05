93ded6860666b75ce1515d9147e4a8cf2c3a3327
drmohammadbarakat <mbarakat@consiliumadvisory.me>
2026-01-05 17:31:50 +0400

    Update app.js

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
   });
 
+console.log("PORT_ENV =", process.env.PORT, "PORT_USED =", PORT);
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
