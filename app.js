f22cdbb33cc9d827d14c95f19238d170a59bd484
drmohammadbarakat <mbarakat@consiliumadvisory.me>
2026-01-05 19:06:46 +0400

    Update app.js

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use((req, res, next) => {
   });
 
 console.log("PORT_ENV =", process.env.PORT, "PORT_USED =", PORT);
+console.log("DEPLOY_MARKER:", process.env.RAILWAY_GIT_COMMIT_SHA || process.env.GITHUB_SHA || "no-sha");
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
